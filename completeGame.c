3c585219ba8f907c57319b36fcee793fc4ba2f58
UMR20170827 <59525437+UMR20170827@users.noreply.github.com>
2020-01-14 16:03:32 +0900

    Create completeGame.c

---
+++ completeGame.c
@@ -0,0 +1,17 @@
+#include "function.h"
+
+void GameOver() {
+	printf("\n\n\n\n\n\n\n\n   GAME OVER.   ");
+	system("pause");
+	system("cls");
+}
+
+void GameClear() {
+	printf("\n\n\n\n\n\n\n\nGAME CLEAR!   ");
+	system("pause");
+	system("cls");
+}
+void stone() {
+	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+}
+
